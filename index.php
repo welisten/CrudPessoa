1f84945498fe1cfa5d18fd5f5a50c16aef499c71
Wesley Welisten <welistenw@gmail.com>
2023-04-21 10:10:14 -0300

    atualização 21.04.23 v1.0

--- index.php
+++ index.php
@@ -9,21 +9,115 @@ $p = new Pessoa($servidor, $bancoDados, $usuario, $senha);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>CRUD-PESSOA</title>
 </head>
 <body>
+    <?php
+        //-------------------- EXCLUIR ---------------------
+        if(isset($_GET['id']))//verifica se a pessoa clicou no botão de excluir
+        {
+            $id_pessoa = addslashes($_GET['id']);
+            $p->excluirPessoa($id_pessoa);
+            header("location: index.php");
+        }
+        if(isset($_POST['nome']))// Verifica se a pessoa clicou no botão cadastrar ou editar
+    {
+        //-------------------- EDITAR ---------------------
+        if(isset($_GET['id_up']) && !empty($_GET['id_up']))
+        {
+            $id_up      = addslashes($_GET['id_up']);
+            $nome       = addslashes($_POST['nome']);
+            $telefone   = addslashes($_POST['telefone']); 
+            $email      = addslashes($_POST['email']);
+            
+            // verificar se as variaveis estão vazias, tornando-as assim, obrigatórias
+            if( !empty($nome) && !empty($telefone) && !empty($email))
+            {
+                // ATUALIZAR
+                $p->altualizarDados($id_up, $nome, $telefone, $email);
+                header("location: index.php");
+            }
+            else
+            {
+                ?>
+                <div class="aviso">
+                    <img src="imgs/aviso.svg" alt="Alarme">
+                    <h4>Preencha todos os campos!</h4>
+                </div>
+                <?php
+            }
+        }
+        //------------------ CADASTRAR ---------------------
+        else
+        {
+            $nome       = addslashes($_POST['nome']);
+            $telefone   = addslashes($_POST['telefone']); 
+            $email      = addslashes($_POST['email']);
+
+            // verificar se as variaveis estão vazias, tornando-as assim, obrigatórias
+            if( !empty($nome) && !empty($telefone) && !empty($email))
+            {
+
+                if(!$p->cadastrarPessoa($nome, $telefone, $email))
+                {
+                    ?>
+                    <div class="aviso">
+                        <img src="imgs/aviso.svg" alt="Alarme">
+                        <h4>Email já está cadastrado</h4>
+                    </div>
+                    <?php
+                }    
+            }
+            else
+            {  
+                
+                ?>
+                <div class="aviso">
+                    <img src="imgs/aviso.svg" alt="Alarme">
+                    <h4>Preencha todos os campos!</h4>
+                </div>
+                <?php
+            }
+        }   
+    }
+    ?>
+    <?php 
+        if(isset($_GET['id_up']))// Verifica se a pessoa clicou no botão editar
+        {
+            $id_up_pessoa = addslashes($_GET['id_up']);
+            $resp = $p->buscarDadosPessoa($id_up_pessoa);
+        }
+    ?>
     <section class="esquerda">
        <div class="esquerda_container">
+            
             <h2 class="form_title">cadastrar pessoa</h2>
-            <form action="">
+            
+            <form method="POST">
                 <label for="nome">nome</label>
-                <input type="text" name="nome" id = "" placeholder="Digite seu Nome" >
+                <input  type="text" 
+                        value="<?php if(isset($resp)){echo $resp['nome'];} ?>"
+                        name="nome" 
+                        id = "nome" 
+                        placeholder="Digite seu Nome" >
+                
                 <label for="telefone">telefone</label>
-                <input type="text" name="telefone" id = "" placeholder="(xx) xxxxx-xxxx" >
+                <input  type="text"
+                        value="<?php if(isset($resp)){echo $resp['telefone'];} ?>" 
+                        name="telefone"
+                        id = "telefone" 
+                        placeholder="(xx) xxxxx-xxxx" >
+                
                 <label for="email">email</label>
-                <input type="text" name="email" id = "" placeholder="exemploemail@email.com">
-                <input type="submit" value="cadastrar">
+                <input  type="text"
+                        value="<?php if(isset($resp)){echo $resp['email'];} ?>" 
+                        name="email" 
+                        id = "email" 
+                        placeholder="exemploemail@email.com">
+                
+                <input  type="submit" 
+                        value="<?php if(isset($resp)){echo "Atualizar";}else{echo "Cadastrar";} ?>">
             </form>
        </div>
     </section>
@@ -50,18 +144,32 @@ $p = new Pessoa($servidor, $bancoDados, $usuario, $senha);
                                 }
                             }
                             ?> 
-                                <td class="buttons">
-                                    <div class="btn"><a href="#">editar</a></div>
-                                    <div class="btn"><a href="#">excluir</a></div>    
-                                </td>
+                        <td class="buttons">
+
+                            <div class="btn">
+                                <a href="index.php?id_up=<?php echo $dados[$i]['id']; ?>">editar</a>
+                            </div>
+                            <div class="btn">
+                                <a href="index.php?id=<?php echo $dados[$i]['id']; ?>">excluir</a>
+                            </div>    
+
+                        </td>
                             <?php
-                            echo"</tr>";          
+                                 echo"</tr>";          
                         }
                     }
-                ?>
+                    else// O BANCO ESTÁ VAZIO
+                    {
+                    ?>
             </table>
+            
+            <div class="aviso">
+            <h4>Ainda não há pessoas cadastradas</h4>
+            </div>
+            <?php
+        }
+         ?>
         </div>
     </section>
-    
 </body>
 </html>
